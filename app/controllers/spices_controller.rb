9effba83d05c3116e14d278b5fe62ed73dff4c85
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-01 02:28:49 +0300

    complete

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,46 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def show 
+        spice = Spice.find_by(id: params[:id])
+        if spice 
+            render json: spice, status: :ok
+        else 
+            render json: { error: "Spice nof found" }, status: :not_found
+        end
+    end
+
+    def create 
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update 
+        spice = Spice.find_by(id: params[:id])
+        if spice.update(spice_params)
+        render json: spice, status: :accepted
+        else 
+            render json: { error: "Spice not found" }, status: :nof_found
+        end
+
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        if spice.destroy
+            head :no_content
+        else
+            render json: { error: "Spice nof found" }, status: :not_found
+        end
+    end
+
+
+    private 
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+    
+end
